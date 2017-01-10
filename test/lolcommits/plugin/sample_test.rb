309fac31ee19c1af91285070012554a359f43b64
Matthew Hutchinson <matt@hiddenloop.com>
2017-01-10 22:42:22 +0000

    some placeholder tests added

--- test/lolcommits/plugin/sample_test.rb
+++ test/lolcommits/plugin/sample_test.rb
@@ -1,8 +1,42 @@
 require 'test_helper'
 
-class Lolcommits::Plugin::SampleTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Lolcommits::Sample::VERSION
-    refute_nil ::Lolcommits::Plugin::Sample::VERSION
+describe Lolcommits::Plugin::Sample do
+
+  it 'should have a version number' do
+    ::Lolcommits::Plugin::Sample::VERSION.wont_be_nil
+  end
+
+  it 'should have a name' do
+    ::Lolcommits::Plugin::Sample.name.must_equal 'plugin-sample'
+  end
+
+  describe 'with a runner' do
+    def plugin
+      @plugin ||= Lolcommits::Plugin::Sample.new(OpenStruct.new)
+    end
+
+    describe 'capturing' do
+      it 'should execute on post capture' do
+      end
+
+      it 'should puts a message after capturing' do
+      end
+    end
+
+    describe 'enabled' do
+      it 'should be disabled by default' do
+      end
+    end
+
+    describe 'configuring' do
+      it 'should not be configured by default' do
+      end
+
+      it 'should check for a valid configuration' do
+      end
+
+      it 'should allow options to be configured' do
+      end
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'lolcommits/plugin/sample'
 
+require 'lolcommits/sample'
+require 'lolcommits/plugin/sample'
 require 'minitest/autorun'
